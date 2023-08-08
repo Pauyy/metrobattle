8f617a94797e0c8f643a63b55935b54b10372ebc
Pauyy <pauyyk@gmail.com>
2023-08-08 13:08:14 +0200

    check if pokemon to tera is a mega pokemon and don't tera it

--- metro.js
+++ metro.js
@@ -36,10 +36,13 @@ class Battle{
 		this.id = null;
 		this.alive = [true, true];
 		this.tera = false;
+		this.mega = [false, false];
 	}
 	reset(){
 		this.alive[0] = true;
 		this.alive[1] = true;
+		this.mega[0] = false;
+		this.mega[1] = false;
 		this.id = null;
 		this.playerNumber = null;
 		this.tera = false;
@@ -103,8 +106,8 @@ function login(challstr){
 }
 
 function attack(){
-	const t1 = !b.tera && pokemon_to_tera == 1 ? "terastallize" : "";
-	const t2 = !b.tera && pokemon_to_tera == 2 ? "terastallize" : "";
+	const t1 = !b.tera && pokemon_to_tera == 1 && !b.mega[0] ? "terastallize" : "";
+	const t2 = !b.tera && pokemon_to_tera == 2 && !b.mega[1] ? "terastallize" : "";
 	const pokemon1 = b.alive[0] ? "move 1" : "pass";
 	const pokemon2 = b.alive[1] ? "move 1" : "pass";
 	console.log(`${b.id}|/choose ${pokemon1} ${t1}, ${pokemon2} ${t2}`);
@@ -139,6 +142,17 @@ function finishBattle(){
 function handlePlayer(action){ 
 	if(action[3] === `${username}`){
 		b.playerNumber = action[2];
+	}
+}
+
+//When check which pokemon mega pokemon and can't tera
+function handleSwitch(){
+	if(action[2].substring(0,2) === b.playerNumber){
+		if(action[2].substring(2,3) === "a"){
+			b.mega[0] = action[3].includes("-Mega") ? true : false
+		} else {
+			b.mega[1] = action[3].includes("-Mega") ? true : false
+		} 
 	}
 }
 
@@ -203,6 +217,8 @@ function handleMessage(data){
 	} else if(action[1] === "nametaken") {
 		console.log(data.toString())
 		process.exit(-1);
+	} else if (action[1] === "switch") {
+		handleSwitch(action);
 	} else if(action[1] === "popup") {
 		console.log("\x1b[31m|Showdown Popup|\x1b[0m", action.slice(2).join());
 		handlePopUp(action)
