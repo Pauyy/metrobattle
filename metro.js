aa5c65061d057216499277888720c28b03e9001a
Pauyy <pauyyk@gmail.com>
2023-08-18 17:27:24 +0200

    Remove debug log

--- metro.js
+++ metro.js
@@ -238,7 +238,6 @@ function handlePrivateMessage(action){
 	//substring(1) because it has a trailing space
 	if(action[2].substring(1) === username){ //if it is a message we send ourself we ignore it
 		if(/<a href="([^"]+)">/.test(action[4])){//except they contain the battle link
-			console.log("set game true")
 			getBattleById(action[4].match(/<a href="([^"]+)">/)[1].substring(1)).priv_challenge = true;
 		}
 		return;
